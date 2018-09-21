602042245ac2a9d0e5a7e937cf2e50003634b1a3
Richard B <C:\Users\dmitr\AppData\Roaming\The Bat!>
2018-09-21 14:22:42 +0300

    firstTry

--- src/index.js
+++ src/index.js
@@ -3,5 +3,15 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
-};
+  let result = 0; // your implementation
+  let length = preferences.length;
+  for(let i = 0; i < length; i++) { 
+    let a = preferences[i] - 1;
+    let b = preferences[a] - 1;
+    let c = preferences[b] - 1;
+      if (a !== b && b !== c && c === i) {
+        result++;
+      }
+}
+return result / 3;
+}
